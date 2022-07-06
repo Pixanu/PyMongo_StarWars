6f5b071701fedae52dce0faf66f83663792f470c
pixanu <4A2HavuH4Si6gRF>
2022-07-06 13:15:28 +0100

    Function to loop through the ships from the api # Function not returning the last page (*NEED TO FIX*)

--- Starwars/main.py
+++ Starwars/main.py
@@ -14,19 +14,38 @@ import pymongo
 import requests
 from pprint import pprint
 
+def add_into_database(api_starwars):
+     db["starships"].insert_many(api_starwars)
 
+def ships_from_swapi(api_starwars):
+     ships_list = []
+     while api_starwars["next"] != None:
+          # Loop through the ships list and add them to an empty list
+          for each_ship in api_starwars["results"]:
+               ships_list.append(each_ship)
+              # If there are no more ships left, break out of
+          # Set a new variable to go to the next page and continue
+          new_page = requests.get(api_starwars["next"])
+          api_starwars = new_page.json()
+         # function is not returning last page
+     # find a way to acces the last page and return the ships_list
 
-
-# def add_into_database(api_starwars):
-#     db["starships"].insert_many(api_starwars)
 
 
 client = pymongo.MongoClient()
 db = client['starwars']
 
 
-swapi = requests.get("https://swapi.dev/api/starships")
+swapi = requests.get("https://swapi.dev/api/starships/?page=1")
 swapi_api_ships = swapi.json()
 pprint(swapi_api_ships)
+
+
+# --------------------------------------------
+# TESTING to see if the api goes to the next page
+# new_page = requests.get(swapi_api_ships["next"])
+# test_page = new_page.json()
+# pprint(test_page)
+
 
 #add_into_database(swapi_api_ships)
